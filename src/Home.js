8600d5e06ad1cdd8e2ddd9df10ddcb1badcf5365
bavshehata <bavshehata@gmail.com>
2021-03-25 14:40:46 +0200

    Use hooks

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,27 @@
+import { useState } from 'react';
+
 const Home = () => {
+  //   let name = 'bavly';
+  const [name, setName] = useState('Bavly');
+  const handleClick = () => {
+    alert('Hello Ninjas');
+    setName('Luigi');
+  };
+  const handleClickAgain = (name) => {
+    alert(`Hello ${name}`);
+  };
   return (
     <div className="home">
       <h1>Dojo Blogs</h1>
-      <p>Where all ninjas write their stuff</p>
+      <p>Where all ninjas write their stuff, right {name}?</p>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={() => {
+          handleClickAgain(name);
+        }}
+      >
+        Click Me Again
+      </button>
     </div>
   );
 };
