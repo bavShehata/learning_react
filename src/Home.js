44f3431f37473e0d524aafbb85a38ca5e03c8c81
bavshehata <bavshehata@gmail.com>
2021-03-25 18:16:43 +0200

    Handling errors

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,31 @@ import axios from 'axios';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
   };
 
   useEffect(() => {
-    axios.get('http://localhost:8001/blogs').then((res) => {
-      setBlogs(res.data);
-      setIsPending(false);
-    });
+    axios
+      .get('http://localhost:8001/blogs')
+      .then((res) => {
+        setBlogs(res.data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((e) => {
+        console.log("Couldn't load blogs\n", e);
+        setError(e.message);
+        setIsPending(false);
+      });
   }, []);
   return (
     <div className="home">
       <h1>Dojo Blogs</h1>
       <p>Where all ninjas write their stuff</p>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList
