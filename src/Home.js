0f62a2c6b1b8098a15df58060954239f7c35778d
bavshehata <bavshehata@gmail.com>
2021-03-25 14:18:38 +0200

    adding templates and components

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Dojo Blogs</h1>
+      <p>Where all ninjas write their stuff</p>
+    </div>
+  );
+};
+
+export default Home;
